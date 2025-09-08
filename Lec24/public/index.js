902404c50710fb82e73e1cfa7fa9e4e984e1adfb
yash01me <yash01.me@gmail.com>
2025-09-08 14:19:09 +0530

    new

---
+++ Lec24/public/index.js
@@ -0,0 +1,58 @@
+
+
+let signupForm=document.querySelector("#signup-form");
+let signupName=document.querySelector("#signup-name");
+let signupEmail=document.querySelector("#signup-email");
+let signupPassword=document.querySelector("#signup-password");
+
+signupForm.addEventListener("submit",async function(e){
+    e.preventDefault();
+    let nameValue=signupName.value;
+    let emailValue=signupEmail.value;
+    let passwordValue=signupPassword.value;
+
+    let res = await fetch("/users",{
+        method:"POST",
+        body:JSON.stringify({
+            username:nameValue,
+            email:emailValue,
+            password:passwordValue
+        }),
+        headers:{"Content-Type":"application/json"}
+    })
+
+    let data = await res.json();
+    console.log(data);
+    if(data.success){
+        alert("signed up, please login " + data.data.username);
+        signupForm.reset();
+    }
+})
+//login 
+let loginForm=document.querySelector("#login-form");
+let loginEmail=document.querySelector("#login-email");
+let loginPassword=document.querySelector("#login-password");
+
+loginForm.addEventListener("submit",async function(e){
+    e.preventDefault();
+  
+    let emailValue=loginEmail.value;
+    let passwordValue=loginPassword.value;
+
+    let res = await fetch("/login",{
+        method:"POST",
+        body:JSON.stringify({
+            email:emailValue,
+            password:passwordValue
+        }),
+        headers:{"Content-Type":"application/json"}
+    })
+
+    let data = await res.json();
+    console.log(data);
+    if(data.success){
+        let token=data.token;
+        localStorage.setItem("token",token);
+        alert("logged in, welcome ");
+    }
+});
